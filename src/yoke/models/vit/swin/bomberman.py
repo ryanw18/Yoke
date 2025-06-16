fixed l2 norm calculation in bomberman.py

src/yoke/models/vit/swin/bomberman.py
@@ -173,7 +173,8 @@ class LodeRunner(nn.Module):
         # of integers corresponding to variables in the `default_vars` list.
 
         # DPOT-style noise injection:
-        l2_norm = torch.norm(x, dim=(1, 2, 3), keepdim=True)
+        # l2_norm = torch.norm(x, dim=(1, 2, 3), keepdim=True)
+        l2_norm = torch.sqrt((x * x).sum(dim=(1, 2, 3), keepdim=True))
         noise = torch.randn_like(x)
         x = x + self.noise_scale * l2_norm * noise
 